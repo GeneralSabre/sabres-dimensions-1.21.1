9a13ff68d1242e4e108814088c2fdadc0124c8a2
General Sabre <sunil@houseofhum.com>
2025-07-09 19:12:20 -0400

    Initial commit

---
+++ src/main/java/net/generalsabre/sabresdimensions/SabresDimensionsClient.java
@@ -0,0 +1,10 @@
+package net.generalsabre.sabresdimensions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SabresDimensionsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
