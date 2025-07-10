ff863ba08771af0813d44d8b874a455ae0321c3f
General Sabre <sunil@houseofhum.com>
2025-07-10 12:37:57 -0400

    added 2 blocks, changed 1 texture

--- src/main/java/net/generalsabre/sabresdimensions/SabresDimensions.java
+++ src/main/java/net/generalsabre/sabresdimensions/SabresDimensions.java
@@ -2,6 +2,7 @@ package net.generalsabre.sabresdimensions;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.generalsabre.sabresdimensions.block.ModBlocks;
 import net.generalsabre.sabresdimensions.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,5 +15,6 @@ public class SabresDimensions implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
