58b08d1cafdd1d49fa475f61941c884eeed5ce23
General Sabre <sunil@houseofhum.com>
2025-07-10 14:39:06 -0400

    added arakite textures and zalant ore texture

--- src/main/java/net/generalsabre/sabresdimensions/block/ModBlocks.java
+++ src/main/java/net/generalsabre/sabresdimensions/block/ModBlocks.java
@@ -14,11 +14,32 @@ import net.minecraft.util.Identifier;
 
 public class ModBlocks {
 
+    // Zalant Blocks
     public static final Block ZALANT_BLOCK = registerBlock("zalant_block",
-            new Block(AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.METAL)));
+            new Block(AbstractBlock.Settings.create().strength(5f,6f).requiresTool().sounds(BlockSoundGroup.METAL)));
 
     public static final Block RAW_ZALANT_BLOCK = registerBlock("raw_zalant_block",
-            new Block(AbstractBlock.Settings.create().strength(3f).requiresTool().sounds(BlockSoundGroup.METAL)));
+            new Block(AbstractBlock.Settings.create().strength(5f,6f).requiresTool().sounds(BlockSoundGroup.METAL)));
+
+    public static final Block ZALANT_ORE = registerBlock("zalant_ore",
+            new Block(AbstractBlock.Settings.create().strength(3f,3f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+
+
+    // Arakite Blocks
+    public static final Block ARAKITE = registerBlock("arakite",
+            new Block(AbstractBlock.Settings.create().strength(2.5f,5f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+
+    public static final Block ARAKITE_BRICKS = registerBlock("arakite_bricks",
+            new Block(AbstractBlock.Settings.create().strength(2.5f,5f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE_BRICKS)));
+
+    public static final Block POLISHED_ARAKITE = registerBlock("polished_arakite",
+            new Block(AbstractBlock.Settings.create().strength(2.5f,5f).requiresTool().sounds(BlockSoundGroup.POLISHED_DEEPSLATE)));
+
+    public static final Block ARAKITE_TILES = registerBlock("arakite_tiles",
+            new Block(AbstractBlock.Settings.create().strength(2.5f,5f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE_TILES)));
+
+    public static final Block CORRODED_ARAKITE = registerBlock("corroded_arakite",
+            new Block(AbstractBlock.Settings.create().strength(2f,3.5f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
 
     private static void registerBlockItem(String name, Block block){
         Registry.register(Registries.ITEM, Identifier.of(SabresDimensions.MOD_ID, name),
@@ -35,6 +56,12 @@ public class ModBlocks {
             ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(fabricItemGroupEntries -> {
             fabricItemGroupEntries.add(ZALANT_BLOCK);
             fabricItemGroupEntries.add(RAW_ZALANT_BLOCK);
+            fabricItemGroupEntries.add(ZALANT_ORE);
+            fabricItemGroupEntries.add(ARAKITE);
+            fabricItemGroupEntries.add(ARAKITE_BRICKS);
+            fabricItemGroupEntries.add(POLISHED_ARAKITE);
+            fabricItemGroupEntries.add(ARAKITE_TILES);
+            fabricItemGroupEntries.add(CORRODED_ARAKITE);
         });
     }
 }
