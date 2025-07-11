2b7c221fe5c4c4e5db1619d0f8ed492d6ea4cd09
General Sabre <sunil@houseofhum.com>
2025-07-10 20:11:30 -0400

    added dalamine and chorazine components

--- src/main/java/net/generalsabre/sabresdimensions/block/ModBlocks.java
+++ src/main/java/net/generalsabre/sabresdimensions/block/ModBlocks.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.generalsabre.sabresdimensions.SabresDimensions;
+import net.minecraft.block.FallingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -17,29 +18,41 @@ public class ModBlocks {
     // Zalant Blocks
     public static final Block ZALANT_BLOCK = registerBlock("zalant_block",
             new Block(AbstractBlock.Settings.create().strength(5f,6f).requiresTool().sounds(BlockSoundGroup.METAL)));
-
     public static final Block RAW_ZALANT_BLOCK = registerBlock("raw_zalant_block",
             new Block(AbstractBlock.Settings.create().strength(5f,6f).requiresTool().sounds(BlockSoundGroup.METAL)));
-
     public static final Block ZALANT_ORE = registerBlock("zalant_ore",
-            new Block(AbstractBlock.Settings.create().strength(3f,3f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+            new Block(AbstractBlock.Settings.create().strength(5f,5f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
 
 
     // Arakite Blocks
     public static final Block ARAKITE = registerBlock("arakite",
             new Block(AbstractBlock.Settings.create().strength(2.5f,5f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
-
     public static final Block ARAKITE_BRICKS = registerBlock("arakite_bricks",
             new Block(AbstractBlock.Settings.create().strength(2.5f,5f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE_BRICKS)));
-
     public static final Block POLISHED_ARAKITE = registerBlock("polished_arakite",
             new Block(AbstractBlock.Settings.create().strength(2.5f,5f).requiresTool().sounds(BlockSoundGroup.POLISHED_DEEPSLATE)));
-
     public static final Block ARAKITE_TILES = registerBlock("arakite_tiles",
             new Block(AbstractBlock.Settings.create().strength(2.5f,5f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE_TILES)));
-
     public static final Block CORRODED_ARAKITE = registerBlock("corroded_arakite",
             new Block(AbstractBlock.Settings.create().strength(2f,3.5f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+
+    // Dalamine Blocks
+    public static final Block DALAMINE_BLOCK = registerBlock("dalamine_block",
+            new Block(AbstractBlock.Settings.create().strength(1.5f).requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+    public static final Block DALAMINE_ORE = registerBlock("dalamine_ore",
+            new Block(AbstractBlock.Settings.create().strength(2.5f,5f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+
+    // Penkazine Blocks
+    public static final Block CHORAZINE_ORE = registerBlock("chorazine_ore",
+            new Block(AbstractBlock.Settings.create().strength(2.5f,5f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+    public static final Block PENKAZ_ORE = registerBlock("penkaz_ore",
+            new Block(AbstractBlock.Settings.create().strength(2.5f,5f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+    public static final Block CHORAZINE_POWDER_BLOCK = registerBlock("chorazine_powder_block",
+            new Block(AbstractBlock.Settings.create().strength(1.5f,1.5f).requiresTool().sounds(BlockSoundGroup.SAND)));
+    public static final Block PENKAZ_BLOCK = registerBlock("penkaz_block",
+            new Block(AbstractBlock.Settings.create().strength(5f,6f).requiresTool().sounds(BlockSoundGroup.METAL)));
+    public static final Block RAW_PENKAZ_BLOCK = registerBlock("raw_penkaz_block",
+            new Block(AbstractBlock.Settings.create().strength(2.5f,5f).requiresTool().sounds(BlockSoundGroup.STONE)));
 
     private static void registerBlockItem(String name, Block block){
         Registry.register(Registries.ITEM, Identifier.of(SabresDimensions.MOD_ID, name),
@@ -62,6 +75,13 @@ public class ModBlocks {
             fabricItemGroupEntries.add(POLISHED_ARAKITE);
             fabricItemGroupEntries.add(ARAKITE_TILES);
             fabricItemGroupEntries.add(CORRODED_ARAKITE);
+            fabricItemGroupEntries.add(DALAMINE_BLOCK);
+            fabricItemGroupEntries.add(DALAMINE_ORE);
+            fabricItemGroupEntries.add(PENKAZ_ORE);
+            fabricItemGroupEntries.add(PENKAZ_BLOCK);
+            fabricItemGroupEntries.add(RAW_PENKAZ_BLOCK);
+            fabricItemGroupEntries.add(CHORAZINE_ORE);
+            fabricItemGroupEntries.add(CHORAZINE_POWDER_BLOCK);
         });
     }
 }
--- src/main/java/net/generalsabre/sabresdimensions/item/ModItems.java
+++ src/main/java/net/generalsabre/sabresdimensions/item/ModItems.java
@@ -9,9 +9,18 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-
+    // ARAKITE
+    public static final Item ARAKITE_ROD = registerItem("arakite_rod", new Item(new Item.Settings()));
+    // ZALANT
     public static final Item ZALANT_INGOT = registerItem("zalant_ingot", new Item(new Item.Settings()));
     public static final Item RAW_ZALANT = registerItem("raw_zalant", new Item(new Item.Settings()));
+    public static final Item ZALANT_ROD = registerItem("zalant_rod", new Item(new Item.Settings()));
+    // DALAMINE
+    public static final Item DALAMINE_CRYSTAL = registerItem("dalamine_crystal", new Item(new Item.Settings()));
+    // PENKAZINE
+    public static final Item CHORAZINE_DUST = registerItem("chorazine_dust", new Item(new Item.Settings()));
+    public static final Item PENKAZ_INGOT = registerItem("penkaz_ingot", new Item(new Item.Settings()));
+    public static final Item RAW_PENKAZ = registerItem("raw_penkaz", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, Identifier.of(SabresDimensions.MOD_ID, name), item);
@@ -24,6 +33,12 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
             fabricItemGroupEntries.add(ZALANT_INGOT);
             fabricItemGroupEntries.add(RAW_ZALANT);
+            fabricItemGroupEntries.add(DALAMINE_CRYSTAL);
+            fabricItemGroupEntries.add(ZALANT_ROD);
+            fabricItemGroupEntries.add(ARAKITE_ROD);
+            fabricItemGroupEntries.add(CHORAZINE_DUST);
+            fabricItemGroupEntries.add(PENKAZ_INGOT);
+            fabricItemGroupEntries.add(RAW_PENKAZ);
         });
     }
 
