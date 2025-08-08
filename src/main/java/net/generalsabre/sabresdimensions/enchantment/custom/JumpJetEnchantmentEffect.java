d772efd4d470484eca650dcf0f139e1bd844371d
General Sabre <sunil@houseofhum.com>
2025-08-08 16:59:12 -0400

    adding enchantment!

--- src/main/java/net/generalsabre/sabresdimensions/enchantment/custom/JumpJetEnchantmentEffect.java
+++ src/main/java/net/generalsabre/sabresdimensions/enchantment/custom/JumpJetEnchantmentEffect.java
@@ -1,16 +1,20 @@
 package net.generalsabre.sabresdimensions.enchantment.custom;
 
 import com.mojang.serialization.MapCodec;
+import net.generalsabre.sabresdimensions.util.ModKeyBindings;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.enchantment.EnchantmentEffectContext;
-import net.minecraft.enchantment.effect.EnchantmentEntityEffect;
 import net.minecraft.enchantment.effect.EnchantmentLocationBasedEffect;
-import net.minecraft.enchantment.effect.entity.SpawnParticlesEnchantmentEffect;
 import net.minecraft.entity.Entity;
-import net.minecraft.particle.ParticleEffect;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.Vec3d;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
+
+
+import javax.swing.text.JTextComponent;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -21,7 +25,7 @@ public record JumpJetEnchantmentEffect() implements EnchantmentLocationBasedEffe
 
     private static final Map<UUID, Boolean> lastOnGroundMap = new HashMap<>();
 
-    private void triggerEnchantmentEffect(ServerPlayerEntity player, int level, Vec3d pos) {
+    private void triggerEnchantmentEffect(Entity player, int level, Vec3d pos) {
         if (level == 1){
             player.addVelocity(0, .5, 0);
         } else if (level == 2){
@@ -31,18 +35,38 @@ public record JumpJetEnchantmentEffect() implements EnchantmentLocationBasedEffe
         }
     }
 
+    //if (!(user instanceof ServerPlayerEntity player)) return;
+    //
+    //        boolean wasOnGround = lastOnGroundMap.getOrDefault(player.getUuid(), false);
+    //        boolean onGroundNow = player.isOnGround();
+    //
+    //        if (!onGroundNow && wasOnGround && player.getVelocity().y > 0) {
+    //            triggerEnchantmentEffect(player, level, pos);
+    //        }
+    //
+    //        lastOnGroundMap.put(player.getUuid(), onGroundNow);
+
+    public static boolean isJumpJetOn;
+
     @Override
     public void apply(ServerWorld world, int level, EnchantmentEffectContext context, Entity user, Vec3d pos, boolean newlyApplied) {
-        if (!(user instanceof ServerPlayerEntity player)) return;
+        
+        ClientTickEvents.END_CLIENT_TICK.register(minecraftClient -> {
+            if (ModKeyBindings.JUMP_JET_KEY.isPressed()){
+                if (isJumpJetOn){
+                    isJumpJetOn = false;
+                } else {
+                    isJumpJetOn = true;
+                }
+            }
+        });
 
-        boolean wasOnGround = lastOnGroundMap.getOrDefault(player.getUuid(), false);
-        boolean onGroundNow = player.isOnGround();
-
-        if (!onGroundNow && wasOnGround && player.getVelocity().y > 0) {
-            triggerEnchantmentEffect(player, level, pos);
+        MinecraftClient client = MinecraftClient.getInstance();
+        if((client.options.jumpKey.isPressed())&&isJumpJetOn){
+            triggerEnchantmentEffect(user, level, pos);
         }
 
-        lastOnGroundMap.put(player.getUuid(), onGroundNow);
+
     }
 
     @Override
