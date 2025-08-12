f0ecc61a1a3424b748b94973695720bfb792fe16
General Sabre <sunil@houseofhum.com>
2025-08-12 14:13:15 -0400

    prep for weapon entity addition

---
+++ src/main/java/net/generalsabre/sabresdimensions/util/ModModelPredicates.java
@@ -0,0 +1,9 @@
+package net.generalsabre.sabresdimensions.util;
+
+import net.minecraft.client.item.ModelPredicateProviderRegistry;
+
+public class ModModelPredicates {
+    public static void registerModelPredicates(){
+
+    }
+}
